80ded9ae45a9bbe02f2aa50879d3a818b7f7bdc1
Lord-Bardo <129301291+Lord-Bardo@users.noreply.github.com>
2023-08-19 00:00:36 -0300

    Finalizado tp.pl

--- src/tp.pl
+++ src/tp.pl
@@ -1,5 +1,47 @@
 
-:- begin_tests(tp1).
-test(un_contador_honesto_puede_trabajar_en_contaduria, nondet):-
-    puedeAndar(roque, contaduria).
-:- end_tests(tp1).
+
+honesto(ana).
+honesto(roque).
+ingeniera(ana).
+contador(roque).
+ambicioso(roque).
+abogada(cecilia).
+
+trabajoEnUtn(roque).
+trabajoEnUtn(cecilia).
+
+experiencia(Persona):-
+    trabajoEnUtn(Persona).
+
+puedeAndar(contaduria,Persona):-
+    contador(Persona),honesto(Persona).
+
+puedeAndar(ventas,Persona):-
+    ambicioso(Persona), experiencia(Persona).
+
+puedeAndar(ventas,lucia).
+
+
+viveMansion(agatha).
+viveMansion(carnicero).
+viveMansion(charles).
+
+odiaA(agatha, Alguien):-
+    viveMansion(Alguien),
+    Alguien \= carnicero.
+
+odiaA(carnicero, Alguien):-
+    odiaA(agatha, Alguien).
+
+odiaA(charles, Alguien):-
+    viveMansion(Alguien),
+    not(odiaA(agatha, Alguien)).
+
+esMasRicoQueAgatha(Alguien):-
+    viveMansion(Alguien),
+    not(odiaA(carnicero, Alguien)).
+
+asesino(agatha, Asesino):-
+    viveMansion(Asesino),
+    odiaA(Asesino, agatha), 
+    not(esMasRicoQueAgatha(Asesino)).
